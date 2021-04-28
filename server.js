047bcaae648c3e4219adf2f4ff9b5221aa664f5c
Danielle Byrne <DanielleByrne421@gmail.com>
2021-04-27 18:12:07 -0700

    why

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(routes);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+//  });
 
 app.listen(PORT, function() {
-    console.log(`Express server is running on http://localhost:${PORT}!`);
+    console.log(`Express server is running on http://localhost:${PORT}`);
   });
   
