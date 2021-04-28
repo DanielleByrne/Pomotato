d3e35502bf1dc20dedd1f6e5c6f5a75b29c30861
Danielle Byrne <DanielleByrne421@gmail.com>
2021-04-27 17:48:12 -0700

    plz

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
