8ff8d4e2bb8b6fb6fb1518ca03e77e51f4aab7f4
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-13 23:14:05 -0700

    started navbar, am going to take a nap and come back

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,7 +3,11 @@ import React from "react";
 
 function Home() {
     return (
-        <div> this be the home page ty </div>
+        <div className="ui container grid"> this be the home page ty
+        <div className="computer only row">
+        <a href="./Home" class="header item">Pomotato</a>
+        </div>
+         </div>
     )
 }
 
